7a99fe29ed6fb2fdf73fb04789d9589ceb2e0e06
Test <test@gmail.com>
2025-05-12 01:08:04 +0200

    Add hotel search without hotel/city name

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -44,10 +44,35 @@ public class HotelSearch {
 
         hotelNames.forEach(System.out::println);
 
-        Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
-        Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
-        Assert.assertEquals("Rose Rayhaan Rotana", hotelNames.get(2));
-        Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
+        Assert.assertEquals(hotelNames.get(0), "Jumeirah Beach Hotel");
+        Assert.assertEquals( hotelNames.get(1), "Oasis Beach Tower");
+        Assert.assertEquals( hotelNames.get(2), "Rose Rayhaan Rotana");
+        Assert.assertEquals( hotelNames.get(3), "Hyatt Regency Perth");
+    }
+
+    @Test
+    public void searchHotelWithoutName() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        // wprowadzenie daty poprzez wpisanie
+        driver.findElement(By.name("checkin")).sendKeys("25/04/2026");
+        // użycie kalendarza
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+
+        WebElement noResultHeading =  driver.findElement(By.xpath("//div[@class='itemscontainer']//h2"));
+        Assert.assertTrue(noResultHeading.isDisplayed());
+        Assert.assertEquals(noResultHeading.getText(),"No Results Found");
+
     }
 
 }
