d986aa87a9a5b448f04e93d404bae8c46b13f699
Test <test@gmail.com>
2025-05-18 20:14:14 +0200

    Reformat and refactor code

--- src/test/java/pl/seleniumdemo/tests/SignUpTest.java
+++ src/test/java/pl/seleniumdemo/tests/SignUpTest.java
@@ -3,7 +3,6 @@ package pl.seleniumdemo.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import pl.seleniumdemo.model.User;
 import pl.seleniumdemo.pages.HotelSearchPage;
 import pl.seleniumdemo.pages.LoggedUserPage;
 import pl.seleniumdemo.pages.SignUpPage;
